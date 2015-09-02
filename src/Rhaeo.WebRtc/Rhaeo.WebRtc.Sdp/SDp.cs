916cd37e366e0933dd5232531b46b0199cb8fbdf
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-09-02 14:53:56 +0200

    Generate random credentials and specify passive setup

--- src/Rhaeo.WebRtc/Rhaeo.WebRtc.Sdp/SDp.cs
+++ src/Rhaeo.WebRtc/Rhaeo.WebRtc.Sdp/SDp.cs
@@ -1,6 +1,7 @@
 ﻿using Rhaeo.WebRtc.Ice;
 using Rhaeo.WebRtc.Sdp.Extensions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Rhaeo.WebRtc.Sdp
@@ -13,6 +14,14 @@ namespace Rhaeo.WebRtc.Sdp
     public Sdp(IIce ice)
     {
       Ice = ice;
+
+      // Generate a cryptographically secure random user name with minimum of 24 bits of randomness which is 4 characters given the 6 bits of randomness per ICE char (alpha, digit, + and / alphabet).
+      UserName = new RandomIceCharString(4);
+
+      // Generate a cryptographically secure random password with minimum of 128 bits of randomness which is 22 characters given the 6 bits of randomness per ICE char (alpha, digit, + and / alphabet).
+      Password = new RandomIceCharString(22);
+
+      Setup = SdpSetup.Passive;
     }
 
     #endregion
@@ -21,11 +30,36 @@ namespace Rhaeo.WebRtc.Sdp
 
     public IIce Ice { get; }
 
+    // TODO: Introduce IIceCharString and IceCharString and restrict the type to these, remove implicit operator at RandomIceCharString to discourage casting, rely on ToString to handle string interpolation needs.
+    public string UserName { get; }
+
+    // TODO: Introduce IIceCharString and IceCharString and restrict the type to these, remove implicit operator at RandomIceCharString to discourage casting, rely on ToString to handle string interpolation needs.
+    public string Password { get; }
+
+    public SdpSetup Setup { get; }
+
     #endregion
 
     #region Methods
 
-    public override string ToString() => $@"
+    public override string ToString()
+    {
+      // https://tools.ietf.org/html/rfc4145#section-4
+      var setupMap = new Dictionary<SdpSetup, string>()
+      {
+        [SdpSetup.Active] = null, // active
+        [SdpSetup.Passive] = "passive",
+        [SdpSetup.ActivePassive] = null, // actpass
+        [SdpSetup.HoldConnection] = null, // holdconn
+      };
+
+      var setup = setupMap[Setup];
+      if (setup == null)
+      {
+        throw new NotImplementedException("Active, active-passive and hold-connection session setup is not supported.");
+      }
+
+      return $@"
 v=0
 o=- 33718892146891840 2 IN IP4 127.0.0.1
 s=-
@@ -34,13 +68,14 @@ a=msid-semantic: WMS
 m=application 54267 DTLS/SCTP 5000
 c=IN IP4 214.25.96.199
 {string.Join(Environment.NewLine, Ice.GetUdpIceCandidates().Select(udpIceCandidate => udpIceCandidate.ToSdpACandidateLineString()))}
-a=ice-ufrag:CBaeAkCWA6iaPjbU
-a=ice-pwd:uh2p6v2YAx71S5BTLySxUVCa
+a=ice-ufrag:{UserName}
+a=ice-pwd:{Password}
 a=fingerprint:sha-256 18:B6:57:DA:92:EE:45:81:65:EB…
-a=setup:actpass
+a=setup:{setup}
 a=mid:data
 a=sctpmap:5000 webrtc-datachannel 1024
-";
+".Trim();
+    }
 
     #endregion
   }
---
+++ src/Rhaeo.WebRtc/Rhaeo.WebRtc.Sdp/SdpSetup.cs
@@ -0,0 +1,28 @@
+﻿namespace Rhaeo.WebRtc.Sdp
+{
+  /// <summary>
+  /// https://tools.ietf.org/html/rfc4145#section-4
+  /// </summary>
+  public enum SdpSetup
+  {
+    /// <summary>
+    /// The endpoint will initiate an outgoing connection.
+    /// </summary>
+    Active,
+
+    /// <summary>
+    /// The endpoint will accept an incoming connection.
+    /// </summary>
+    Passive,
+
+    /// <summary>
+    /// The endpoint is willing to accept an incoming connection or to initiate an outgoing connection.
+    /// </summary>
+    ActivePassive,
+
+    /// <summary>
+    /// The endpoint does not want the connection to be established for the time being.
+    /// </summary>
+    HoldConnection
+  }
+}
