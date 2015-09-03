d683769568d008c0f1cfa16eb5493df2ea3d7858
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-09-03 15:13:30 +0200

    Create a shared project for Universal app commons

---
+++ src/Rhaeo.WebRtc/Rhaeo.Agenda/Views/OfferView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Rhaeo.Agenda.Views
+{
+  public sealed partial class OfferView : Page
+  {
+    #region Constructors
+
+    public OfferView()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+  }
+}
